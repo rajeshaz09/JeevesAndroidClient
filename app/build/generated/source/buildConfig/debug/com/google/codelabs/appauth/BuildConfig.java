9c37e565ce320d639d3d2c1959b4ab4ef55e3f55
Rajesh Aravapalli <rajeshaz09@outlook.com>
2018-08-06 13:56:04 +0530

    reverted

---
+++ app/build/generated/source/buildConfig/debug/com/google/codelabs/appauth/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.codelabs.appauth;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.codelabs.appauth";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
